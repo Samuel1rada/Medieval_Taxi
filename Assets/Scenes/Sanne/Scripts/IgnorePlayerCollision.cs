72b69f9c647ee3a1161dad159c1c0501aa29e670
Captain_Void <sannedeboer931@gmail.com>
2025-03-07 12:03:48 +0100

    Barrel enhancement
    the shattering barrels now have a little bit more of an explosion and the carriage collides less with the barrel pieces

--- Assets/Scenes/Sanne/Scripts/IgnorePlayerCollision.cs
+++ Assets/Scenes/Sanne/Scripts/IgnorePlayerCollision.cs
@@ -4,8 +4,10 @@ public class IgnorePlayer : MonoBehaviour
 {
     private void Start()
     {
-   
+        Physics.IgnoreLayerCollision(1, 22);
+        Physics.IgnoreLayerCollision(2, 22);
         Physics.IgnoreLayerCollision(20, 22);
+        Physics.IgnoreLayerCollision(21, 22);
         
     }
 
