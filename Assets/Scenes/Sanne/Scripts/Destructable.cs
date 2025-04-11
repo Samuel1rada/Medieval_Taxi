1c24ac4f898403a4794fffaa262e00bc0e20e925
Captain_Void <sannedeboer931@gmail.com>
2025-04-11 14:29:01 +0200

    More obstacles and cluttered the map

--- Assets/Scenes/Sanne/Scripts/Destructable.cs
+++ Assets/Scenes/Sanne/Scripts/Destructable.cs
@@ -1,18 +1,28 @@
 using UnityEngine;
 using System.Collections;
 
-
 public class Destructable : MonoBehaviour
 {
     public GameObject BrokenVersion;
     public float destroyDelay = 6f;
+    public float explosionForce = 0.00008f;
 
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Animal") || collision.gameObject.CompareTag("Wagon"))
         {
+            // Instantiate the broken version of the object
             GameObject brokenInstance = Instantiate(BrokenVersion, transform.position, new Quaternion(transform.rotation.x, transform.rotation.y + 90, transform.rotation.z, transform.rotation.w));
-            Destroy(gameObject); 
+
+            // Apply random forces to each piece of the broken object
+            foreach (Rigidbody rb in brokenInstance.GetComponentsInChildren<Rigidbody>())
+            {
+                Vector3 randomDirection = Random.insideUnitSphere;
+                rb.AddForce(randomDirection * explosionForce);
+            }
+
+            // Destroy the original object and the broken instance after a delay
+            Destroy(gameObject);
             Destroy(brokenInstance, destroyDelay);
         }
     }
--- Assets/Scenes/Sanne/Scripts/IgnorePlayerCollision.cs
+++ Assets/Scenes/Sanne/Scripts/IgnorePlayerCollision.cs
@@ -10,5 +10,4 @@ public class IgnorePlayer : MonoBehaviour
         Physics.IgnoreLayerCollision(21, 22);
         
     }
-
 }
